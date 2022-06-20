cfcda38738d8663b19d780ccff4727f19b4fe8e5
vitdias <vitor.azambuja.90@gmail.com>
2022-06-19 23:37:02 -0300

    Blackjack funcionando com random

--- 01 - Blackjack game/script.js
+++ 01 - Blackjack game/script.js
@@ -1,5 +1,5 @@
-let firstCard = 10
-let secondCard = 4
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
 let cards = [firstCard, secondCard] //array - ordered list of items
 let sum = firstCard + secondCard
 let hasBlackJack = false
@@ -8,18 +8,33 @@ let message = document.getElementById("message-El")
 let sumEl = document.getElementById("sum-El")
 let cardsEl = document.getElementById("cards-El")
 
+function getRandomCard() {
+    randomCard = Math.floor( Math.random() * 13 + 1)
+    if (randomCard == 1) {
+        return 11
+    } else if (randomCard == 11 || randomCard == 12 || randomCard == 13) {
+        return 10
+    } else {
+        return randomCard
+    }
+}
+
 function startGame() {
     renderGame()
 }
 
 function renderGame() {
     sumEl.textContent = "Sum: " + sum
-    cardsEl.textContent = "Cards: " + cards[0] + " , " + cards[1]
+    cardsEl.textContent = "Cards: "
+    for ( let i = 0; i < cards.length; i++ ) {
+        cardsEl.textContent += cards[i] + " "
+    }
 
     if (sum <= 20 ) {
         message.innerText = "Do you want to draw a new card?"
     } else if (sum == 21) {
         message.innerText= "Wohoo! You ve got a Blackjack!"
+        hasBlackJack = true
     } else {
         message.innerText = "You are out of the game!"
         isAlive = false
@@ -30,10 +45,10 @@ function renderGame() {
 }
 
 function newCard() {
-    let card = 7
+    let card = getRandomCard()
     cards.push(card)
-    sum += cards[cards.length]
-    // sumEl.textContent = "Sum: " + sum
-    console.log(cards.length)
+    sum += cards[cards.length - 1]
+
     renderGame()
+    console.log(cards[cards.length - 1])
 }
